b78a085ff9fbe928aedacf8fc957601cab730c53
byhoson <bhson99@gmail.com>
2018-08-25 03:15:50 +0900

    a

--- server.js
+++ server.js
@@ -4,15 +4,21 @@ const app = express()
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
 
 app.listen(3000, () => {
     console.log('server listening')
 })
 
 app.get('/', (req, res) => {
-    res.render('index.html')
+    res.render('index.ejs', { num: 1 })
+})
+
+app.get('/:lng/:lat', (req, res) => {
+    res.render('index.ejs', { num: 3 })
+    //res.send('lng: ' + req.params.lng + ' lat: ' + req.params.lat)
 })
 
 app.post('/', (req, res) => {
-    res.send(req.body.lat)
+    res.redirect('/' + req.body.lng + '/' + req.body.lat)
 })
